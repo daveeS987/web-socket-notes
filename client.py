ec73564941ad5d397e39a76d61205a057fd1b836
Davee Sok <daveesok@gmail.com>
2021-08-13 01:51:02 -0500

    added json

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # Importing the relevant libraries
 import websockets
 import asyncio
+import json
 
 # The main function that will handle connection and communication
 # with the server
@@ -9,9 +10,18 @@ async def listen():
     # Connect to the server
     async with websockets.connect(url) as ws:
         # Send a greeting message
-        await ws.send("Hello Server!")
+        # await ws.send("Hello Server!")
+
+        obj1 = {"hello": "davee", "hi": 123, 1: False, "bye": [1, "2", 3]}
+        converted = json.dumps(obj1)
+        print("converted: ", converted)
         # Stay alive forever, listening to incoming msgs
+
         while True:
+            response = input("> Person1: ")
+            if response == "q":
+                break
+            await ws.send(converted)
             msg = await ws.recv()
             print(msg)
 
--- serverEcho.py
+++ serverEcho.py
@@ -1,9 +1,10 @@
+# Communicates with a client and server
 # Importing the relevant libraries
+import json
 import websockets
 import asyncio
 
 PORT = 7890
-
 print("Server listening on Port " + str(PORT))
 
 
@@ -11,7 +12,9 @@ async def echo(websocket, path):
     print("A client just connected")
     try:
         async for message in websocket:
-            print("Received message from client: " + message)
+            print("message: ", message)
+            converted = json.loads(message)
+            print("converted: ", converted)
             await websocket.send("Pong: " + message)
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
